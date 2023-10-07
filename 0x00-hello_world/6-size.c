860a66f43c020ed69a7fa68352334a0a6e33f213
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-06 18:29:33 -0700

    'add 6-size.c'

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - entry point of the program
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	int c;
+	int i;
+	int li;
+	int lli;
+	int f;
+
+	"Size of a char: %i byte(s)", c
+	"Size of an int: %i byte(s)", i
+	"Size of a long int: %i byte(s)", li
+	"Size of a long long int: %i byte(s)", lli
+	"Size of a float: %i byte(s)", f
+	return (0);
+}
