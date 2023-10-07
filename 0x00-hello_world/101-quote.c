9c01b64a6bd7943ea50c1d82388adc6edfdce4b9
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-06 19:03:26 -0700

    add test files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
-*main - entry point of the program 
+*main - entry point of the program
 *
 *Return: 1 is the return value
 */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char sent[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", sent);
 	return (1);
 }
