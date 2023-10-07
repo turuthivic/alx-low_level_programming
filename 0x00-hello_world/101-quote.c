d63eadbc25a5b17db3e4c309ecd50a0b2195d3ea
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-06 18:53:51 -0700

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry point of the program 
+*
+*Return: 1 is the return value
+*/
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
