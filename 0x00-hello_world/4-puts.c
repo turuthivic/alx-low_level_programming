3957a7ee6465611db6031b2a8f9fa47465a5039b
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-06 18:04:41 -0700

    make corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+* entry point function
+* function that outputs a string and returns void
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
