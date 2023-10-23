88646be83e9c77e47cd14138f74618e2b974139c
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-22 21:30:40 -0700

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry function
+*
+* Return: returns 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
