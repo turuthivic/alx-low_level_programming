d519947d64d838a3a576307701d4578d5f0a1488
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-22 21:53:13 -0700

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - entry point function
+*
+* Return: return 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
