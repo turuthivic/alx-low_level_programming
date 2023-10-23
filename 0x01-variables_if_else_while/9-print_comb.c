21c1a15d055ed08d4ebd6de41c26d4bb704d54cf
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-23 11:06:34 -0700

    add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point function
+*
+* Return: return 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num >= 9)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
