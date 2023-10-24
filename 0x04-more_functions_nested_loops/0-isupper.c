794d172cc93fc682e5469333804fcec8b61dd203
Victor Turuthi <turuthivictor9@gmail.com>
2023-10-24 10:13:59 -0700

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - print uppercase letters
+ * @c: argument character to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
